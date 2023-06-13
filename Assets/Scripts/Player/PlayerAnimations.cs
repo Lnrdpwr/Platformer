93231eca02dd84ec22537ce0b016f57718d526e0
Lnrdpwr <98341880+Lnrdpwr@users.noreply.github.com>
2023-06-13 23:44:59 +0500

    Update PlayerAnimations.cs

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -2,20 +2,25 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
+    //Скрипт отвечает за анимации
     private Animator _playerAnimator;
 
     private void Start()
     {
-        _playerAnimator = GetComponent<Animator>();
+        _playerAnimator = GetComponent<Animator>();//Получаем аниматор игрока
     }
 
     public void SetMoveAnimation(bool isMoving)
     {
-        _playerAnimator.SetBool("IsMoving", isMoving);
+        _playerAnimator.SetBool("IsMoving", isMoving);//Изменяем значение, управляющее анимацией ходьбы(если истина - игрок идёт, если ложь - стоит)
     }
 
     public void SetJumpingAnimation(bool isJumping)
     {
-        _playerAnimator.SetBool("IsJumping", isJumping);
+        _playerAnimator.SetBool("IsJumping", isJumping);//Изменяем значение, управляющее анимацией прыжка(истина - игрок прыгает, ложь - нет)
     }
 }
+
+/*В окне аниматора(windows -> animations -> animator) есть все анимации, переходы и значения,
+которые влияют на переход анимаций
+ */
